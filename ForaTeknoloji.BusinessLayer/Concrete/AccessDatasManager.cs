ba38eb28383be950158a547a88d7527320f2d542
Coşkun KARAKOÇ <coskunkarakocc@gmail.com>
2019-08-23 17:04:44 +0300

    Datepicker-Table-Satir-Click-Event

--- ForaTeknoloji.BusinessLayer/Concrete/AccessDatasManager.cs
+++ ForaTeknoloji.BusinessLayer/Concrete/AccessDatasManager.cs
@@ -51,11 +51,9 @@ namespace ForaTeknoloji.BusinessLayer.Concrete
             return _accessDatasDal.GetList().Select(x => x.Gecis_Tipi).ToList();
         }
 
-        public List<AccessDatas> GetTanimsizListesi(bool? Kapi1, bool? Kapi2, bool? Kapi3, bool? Kapi4, bool? Kapi5, bool? Kapi6, bool? Kapi7, bool? Kapi8, bool? Tümü, bool? TümPanel, int? Panel, DateTime? Tarih1,DateTime? Tarih2, DateTime? Saat1, DateTime? Saat2,string KapiYon)
+        public List<AccessDatas> GetTanimsizListesi(bool? Kapi1, bool? Kapi2, bool? Kapi3, bool? Kapi4, bool? Kapi5, bool? Kapi6, bool? Kapi7, bool? Kapi8, bool? Tümü, bool? TümPanel, int? Panel, DateTime? Tarih1, DateTime? Tarih2, DateTime? Saat1, DateTime? Saat2, string KapiYon)
         {
-            //deneme
-            GetDigerGecisListesi(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,null);
-            //deneme
+
             var liste = _accessDatasDal.GetTanimsizListesi();
             if (Panel != null)
             {
@@ -65,68 +63,6 @@ namespace ForaTeknoloji.BusinessLayer.Concrete
             {
                 liste = liste.Where(x => x.Kapi_ID == 1);
 
-            }
-            if (Kapi2 != null)
-            {
-                liste = liste.Where(x => x.Kapi_ID == 2);
-            }
-            if (Kapi3 != null)
-            {
-                liste = liste.Where(x => x.Kapi_ID == 3);
-            }
-            if (Kapi4 != null)
-            {
-                liste = liste.Where(x => x.Kapi_ID == 4);
-            }
-            if (Kapi5!=null)
-            {
-                liste = liste.Where(x => x.Kapi_ID == 5);
-            }
-            if (Kapi6 != null)
-            {
-                liste = liste.Where(x => x.Kapi_ID == 6);
-            }
-            if (Kapi7 != null)
-            {
-                liste = liste.Where(x => x.Kapi_ID == 7);
-            }
-            if (Kapi8 != null)
-            {
-                liste = liste.Where(x => x.Kapi_ID == 8);
-            }
-            if (KapiYon== "giris")
-            {
-                liste = liste.Where(x => x.Gecis_Tipi == 0);
-            }
-            if (KapiYon=="cikis")
-            {
-                liste = liste.Where(x => x.Gecis_Tipi == 1);
-            }
-            if (Tarih1!=null)
-            {
-                liste = liste.Where(x => x.Tarih >= Tarih1);
-            }
-            if (Tarih1 !=null && Tarih2 !=null)
-            {
-                liste = liste.Where(x => x.Tarih >= Tarih1 && x.Tarih <= Tarih2);  
-            }
-            if (Saat1!=null)
-            {
-                liste = liste.Where(x => x.Tarih >= Saat1);
-            }
-            if (Saat1!=null && Saat2!=null)
-            {
-                liste = liste.Where(x => x.Tarih >= Saat1 && x.Tarih <= Saat2);
-            }
-            return liste.OrderByDescending(x=>x.Kayit_No).ToList();
-        }
-
-        public List<DigerGecisRaporList> GetDigerGecisListesi(bool? Kapi1, bool? Kapi2, bool? Kapi3, bool? Kapi4, bool? Kapi5, bool? Kapi6, bool? Kapi7, bool? Kapi8, bool? TümPanel, int? Paneller, DateTime? Tarih1, DateTime? Tarih2, DateTime? Saat1, DateTime? Saat2, string Tetikleme, string KapiYon)
-        {
-            var liste = _accessDatasDal.GetDigerGecisListesi();
-           if (Kapi1!=null)
-            {
-                liste = liste.Where(x => x.Kapi_ID==1);
             }
             if (Kapi2 != null)
             {
@@ -156,61 +92,139 @@ namespace ForaTeknoloji.BusinessLayer.Concrete
             {
                 liste = liste.Where(x => x.Kapi_ID == 8);
             }
-            if (Paneller!=null)
-            {
-                liste = liste.Where(x => x.Panel_ID == Paneller);
-            }
-            if (KapiYon=="giris")
+            if (KapiYon == "giris")
             {
                 liste = liste.Where(x => x.Gecis_Tipi == 0);
             }
-            if (KapiYon=="cikis")
+            if (KapiYon == "cikis")
             {
                 liste = liste.Where(x => x.Gecis_Tipi == 1);
             }
-            if (Tetikleme== "OTetikleme")
+            if (Tarih1 != null && Tarih2 == null)
             {
-                liste = liste.Where(x => x.TKod==5);
+                liste = liste.Where(x => x.Tarih >= Tarih1);
             }
-            if (Tetikleme== "OAcma")
+            if (Tarih1 != null && Tarih2 != null)
             {
-                liste = liste.Where(x => x.TKod==6);
+                liste = liste.Where(x => x.Tarih >= Tarih1 && x.Tarih <= Tarih2);
+            }
+            if (Saat1 != null && Saat2 == null)
+            {
+                liste = liste.Where(x => x.Tarih >= Saat1);
+            }
+            if (Saat1 != null && Saat2 != null)
+            {
+                liste = liste.Where(x => x.Tarih >= Saat1 && x.Tarih <= Saat2);
+            }
+            return liste.OrderByDescending(x => x.Kayit_No).ToList();
+        }
+
+        public List<DigerGecisRaporList> GetDigerGecisListesi(bool? Kapi1, bool? Kapi2, bool? Kapi3, bool? Kapi4, bool? Kapi5, bool? Kapi6, bool? Kapi7, bool? Kapi8, bool? TümPanel, int? Paneller, DateTime? Tarih1, DateTime? Tarih2, DateTime? Saat1, DateTime? Saat2, string Tetikleme, string KapiYon)
+        {
+            var liste = _accessDatasDal.GetDigerGecisListesi();
+            if (Kapi1 != null)
+            {
+                liste = liste.Where(x => x.Kapi_ID == 1);
+            }
+            if (Kapi2 != null)
+            {
+                liste = liste.Where(x => x.Kapi_ID == 2);
+            }
+            if (Kapi3 != null)
+            {
+                liste = liste.Where(x => x.Kapi_ID == 3);
+            }
+            if (Kapi4 != null)
+            {
+                liste = liste.Where(x => x.Kapi_ID == 4);
+            }
+            if (Kapi5 != null)
+            {
+                liste = liste.Where(x => x.Kapi_ID == 5);
+            }
+            if (Kapi6 != null)
+            {
+                liste = liste.Where(x => x.Kapi_ID == 6);
+            }
+            if (Kapi7 != null)
+            {
+                liste = liste.Where(x => x.Kapi_ID == 7);
+            }
+            if (Kapi8 != null)
+            {
+                liste = liste.Where(x => x.Kapi_ID == 8);
+            }
+            if (Paneller != null)
+            {
+                liste = liste.Where(x => x.Panel_ID == Paneller);
+            }
+            if (KapiYon == "giris")
+            {
+                liste = liste.Where(x => x.Gecis_Tipi == 0);
+            }
+            if (KapiYon == "cikis")
+            {
+                liste = liste.Where(x => x.Gecis_Tipi == 1);
+            }
+            if (Tetikleme == "OTetikleme")
+            {
+                liste = liste.Where(x => x.TKod == 5);
+            }
+            if (Tetikleme == "OAcma")
+            {
+                liste = liste.Where(x => x.TKod == 6);
             }
             if (Tetikleme == "OKapatma")
             {
-                liste = liste.Where(x => x.TKod==7);
+                liste = liste.Where(x => x.TKod == 7);
             }
             if (Tetikleme == "OSerbest")
             {
-                liste = liste.Where(x => x.TKod==13);
+                liste = liste.Where(x => x.TKod == 13);
             }
             if (Tetikleme == "BTetikleme")
             {
-                liste = liste.Where(x => x.TKod==8);
+                liste = liste.Where(x => x.TKod == 8);
             }
             if (Tetikleme == "PAcma")
             {
-                liste = liste.Where(x => x.TKod==9);
+                liste = liste.Where(x => x.TKod == 9);
             }
             if (Tetikleme == "PKapatma")
             {
-                liste = liste.Where(x => x.TKod==10);
+                liste = liste.Where(x => x.TKod == 10);
             }
             if (Tetikleme == "PSerbest")
             {
-                liste = liste.Where(x => x.TKod==14);
+                liste = liste.Where(x => x.TKod == 14);
             }
             if (Tetikleme == "Alarm")
             {
-                liste = liste.Where(x => x.TKod==20);
+                liste = liste.Where(x => x.TKod == 20);
             }
             if (Tetikleme == "Yangin")
             {
-                liste = liste.Where(x => x.TKod==21);
+                liste = liste.Where(x => x.TKod == 21);
             }
-            if (Tetikleme== "KAlarm")
+            if (Tetikleme == "KAlarm")
             {
-                liste = liste.Where(x => x.TKod==22 || x.TKod==23 || x.TKod==24);
+                liste = liste.Where(x => x.TKod == 22 || x.TKod == 23 || x.TKod == 24);
+            }
+            if (Tarih1 != null && Tarih2 == null)
+            {
+                liste = liste.Where(x => x.Tarih >= Tarih1);
+            }
+            if (Tarih1 != null && Tarih2 != null)
+            {
+                liste = liste.Where(x => x.Tarih >= Tarih1 && x.Tarih <= Tarih2);
+            }
+            if (Saat1 != null && Saat2 == null)
+            {
+                liste = liste.Where(x => x.Tarih >= Saat1);
+            }
+            if (Saat1 != null && Saat2 != null)
+            {
+                liste = liste.Where(x => x.Tarih >= Saat1 && x.Tarih <= Saat2);
             }
             var sonuc = liste.ToList();
             return sonuc;
