9a30abd41a2ee4db66d41f6be800e00cbb007715
Coşkun KARAKOÇ <coskunkarakocc@gmail.com>
2020-02-10 13:31:12 +0300

    Upgrade-AktifReport-10-02-2020-13-31

--- ForaTeknoloji.BusinessLayer/Concrete/ReportManager.cs
+++ ForaTeknoloji.BusinessLayer/Concrete/ReportManager.cs
@@ -1168,10 +1168,10 @@ namespace ForaTeknoloji.BusinessLayer.Concrete
                 {
                     queryString += " AND AccessDatas.[User Kayit No] =" + parameters.User;
                 }
-                else
-                {
-                    queryString += " AND AccessDatas.[User Kayit No] =" + 0;
-                }
+                //else
+                //{
+                //    queryString += " AND AccessDatas.[User Kayit No] =" + 0;
+                //}
             }
             if (parameters.Kapi_Yon == 0)
             {
@@ -1181,31 +1181,31 @@ namespace ForaTeknoloji.BusinessLayer.Concrete
             {
                 queryString += " AND AccessDatas.[Gecis Tipi] = 1";
             }
-            if (parameters.Sirket != null)
+            if (parameters.Sirket != null && parameters.Sirket != 0)
             {
                 queryString += " AND Users.[Sirket No] =" + parameters.Sirket;
             }
-            if (parameters.Departman != null)
+            if (parameters.Departman != null && parameters.Departman != 0)
             {
                 queryString += " AND Users.[Departman No] =" + parameters.Departman;
             }
-            if (parameters.AltDepartman != null)
+            if (parameters.AltDepartman != null && parameters.AltDepartman != 0)
             {
                 queryString += " AND Users.[Alt Departman No] =" + parameters.AltDepartman;
             }
-            if (parameters.Bolum != null)
+            if (parameters.Bolum != null && parameters.Bolum != 0)
             {
                 queryString += " AND Users.[Bolum No] =" + parameters.Bolum;
             }
-            if (parameters.Unvan != null)
+            if (parameters.Unvan != null && parameters.Unvan != 0)
             {
                 queryString += " AND Users.[Unvan No] =" + parameters.Unvan;
             }
-            if (parameters.Blok != null)
+            if (parameters.Blok != null && parameters.Blok != 0)
             {
                 queryString += " AND Users.[Blok No] =" + parameters.Blok;
             }
-            if (parameters.Gecis_Grubu != null)
+            if (parameters.Gecis_Grubu != null && parameters.Gecis_Grubu != 0)
             {
                 queryString += " AND Users.[Grup No] =" + parameters.Gecis_Grubu;
             }
@@ -1381,10 +1381,10 @@ namespace ForaTeknoloji.BusinessLayer.Concrete
                 {
                     queryString += "AND AccessDatas.[User Kayit No] =" + parameters.User;
                 }
-                else
-                {
-                    queryString += "AND AccessDatas.[User Kayit No] =" + 0;
-                }
+                //else
+                //{
+                //    queryString += "AND AccessDatas.[User Kayit No] =" + 0;
+                //}
             }
             if (parameters.Kapi_Yon == 0)
             {
@@ -1394,27 +1394,27 @@ namespace ForaTeknoloji.BusinessLayer.Concrete
             {
                 queryString += " AND AccessDatas.[Gecis Tipi] = 1";
             }
-            if (parameters.Gecis_Grubu != null)
+            if (parameters.Gecis_Grubu != null && parameters.Gecis_Grubu != 0)
             {
                 queryString += " AND UsersOLD.[Grup No] =" + parameters.Gecis_Grubu;
             }
-            if (parameters.Sirket != null)
+            if (parameters.Sirket != null && parameters.Sirket != 0)
             {
                 queryString += " AND UsersOLD.[Sirket No] =" + parameters.Sirket;
             }
-            if (parameters.Departman != null)
+            if (parameters.Departman != null && parameters.Departman != 0)
             {
                 queryString += " AND UsersOLD.[Departman No] =" + parameters.Departman;
             }
-            if (parameters.AltDepartman != null)
+            if (parameters.AltDepartman != null && parameters.AltDepartman != 0)
             {
                 queryString += " AND UsersOLD.[Alt Departman No] =" + parameters.AltDepartman;
             }
-            if (parameters.Bolum != null)
+            if (parameters.Bolum != null && parameters.Bolum != 0)
             {
                 queryString += " AND UsersOLD.[Bolum No] =" + parameters.Bolum;
             }
-            if (parameters.Unvan != null)
+            if (parameters.Unvan != null && parameters.Unvan != 0)
             {
                 queryString += " AND UsersOLD.[Unvan No] =" + parameters.Unvan;
             }
@@ -1422,7 +1422,7 @@ namespace ForaTeknoloji.BusinessLayer.Concrete
             {
                 queryString += " AND UsersOLD.[Plaka] ='" + parameters.Plaka.Trim() + "'";
             }
-            if (parameters.Blok != null)
+            if (parameters.Blok != null && parameters.Blok != 0)
             {
                 queryString += " AND UsersOLD.[Blok No] =" + parameters.Blok;
             }
@@ -1430,7 +1430,7 @@ namespace ForaTeknoloji.BusinessLayer.Concrete
             {
                 queryString += " AND UsersOLD.[Daire] =" + parameters.Daire;
             }
-            if (parameters.Panel != null)
+            if (parameters.Panel != null && parameters.Panel != 0)
             {
                 queryString += " AND AccessDatas.[Panel ID] =" + parameters.Panel;
             }
